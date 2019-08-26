0c7abf6ab3515133f9c5aeab4e3cc3367c7facab
jshepp27 <j.sheppard.ldn@gmail.com>
2019-08-26 10:45:29 +0100

    initial

---
+++ MultiPulz.py
@@ -0,0 +1,80 @@
+# Libraries
+import tweepy
+from tweepy import API
+from tweepy import Cursor
+from tweepy import OAuthHandler
+import datetime, time
+from twitter_credentials import consumer_key, consumer_secret, access_token, access_token_secret
+import csv
+
+#Authenticate Twitter; Return API object. 
+class TwitterAuthenticator():
+
+    def twitter_auth(self):
+        global auth
+        global api
+
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = API(auth)
+        return api
+
+# Extract input file 
+class FileOps():
+
+    def strip_file(self):
+        global username
+        username = []
+
+        with open("user_list.txt", "r") as f:
+            targets_list = f.readlines()
+
+        for i in targets_list:
+            username.append(i.strip('\n'))
+        print(username)
+
+# Download tweets
+class PullTweets():
+    
+    def get_tweets(self, screen_name): 
+
+        print("downloading tweets ...")
+        global tweets
+        tweets = []
+
+        fresh_tweets = api.user_timeline(screen_name, count=5)  
+        oldest_id = fresh_tweets[-1].id
+
+        while len(fresh_tweets) > 0:
+            fresh_tweets = api.user_timeline(screen_name, count=5, since_id = oldest_id)  
+            tweets.extend(fresh_tweets)
+            oldest_id = tweets[-1].id
+            print(f"{len(tweets)} tweets downloaded ...")
+
+        out_tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in tweets]
+        
+        with open(f'{screen_name} tweets.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(["id","created_at","text"])
+            writer.writerows(out_tweets)
+
+    def all_tweets(self):
+        
+        for i in username:
+            self.get_tweets(i)
+
+# Execution 
+
+twitter_authenticator = TwitterAuthenticator()
+twitter_authenticator.twitter_auth()
+
+file_ops = FileOps()
+file_ops.strip_file()
+
+pull_tweets = PullTweets()
+pull_tweets.all_tweets()
+
+
+
+
+
---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+# Credentials
+
+consumer_key = "ZhSPPDmijJ1xsIrfbRqgE5h05"
+consumer_secret = "VauRt0VQdxNbKDOdfvX6vtr9fp83xVKAzWs1Nc2Ykodp084TZ1"
+access_token = "1156898813598212096-efz1EvT985WY1zPBJwsUdI7FkWwknV"
+access_token_secret = "8mgozFcEeoOBfZDvSE9OJf7eacaFDo4AvXPfIAdA34CPO"
+
+file_name = ""
+last_tweet_ID = ""
