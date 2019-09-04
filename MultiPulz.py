83eb380cdeb4dd7b6885b6172f0c6a2d76279079
jshepp27 <j.sheppard.ldn@gmail.com>
2019-09-04 14:24:27 +0100

    initial commit

--- MultiPulz.py
+++ MultiPulz.py
@@ -7,6 +7,8 @@ import datetime, time
 from twitter_credentials import consumer_key, consumer_secret, access_token, access_token_secret
 import csv
 from textblob import TextBlob
+import sqlite3
+import json
 
 #Authenticate Twitter; Return API object. 
 class TwitterAuthenticator():
@@ -34,18 +36,27 @@ class FileOps():
             username.append(i.strip('\n'))
         print(username)
 
+class Tweet():
+
+    # Data on the tweet tweet.id_str, tweet.created_at, tweet.text.encode("utf-8"), self.sentiment(tweet.text)
+    def __init__(self, tweet_id, text, sentiment):
+        self.tweet_id = tweet_id
+        # self.created_at = created_at
+        self.text = text
+        self.sentiment = sentiment
+
+    # Inserting that data into the DB
+    def insertTweet(self):
+
+        c.execute("INSERT INTO tweets (tweet_id, text, sentiment) VALUES (?, ?, ?)",
+            (self.tweet_id, self.text, self.sentiment))
+        conn.commit()
+
 # Download tweets
 class PullTweets():
     
-    # def sentiment(self, text):
-    #     if TextBlob(text).sentiment == 0:
-    #         print("Neutral")
-        
-    #     elif TextBlob(text).sentiment > 0:
-    #         print("Positive")
-
-    #     elif TextBlob(text).sentiment < 0:
-    #         print("Negative")
+    def sentiment(self, text):
+       return TextBlob(text).sentiment
 
     def get_tweets(self, screen_name): 
 
@@ -63,31 +74,46 @@ class PullTweets():
             print(f"{len(tweets)} tweets downloaded ...")
 
         out_tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8"), self.sentiment(tweet.text)] for tweet in tweets]
-        
+
+        for i in tweets:
+            tweet = Tweet(str(i.id_str), str(i.text.encode('utf-8')), str(self.sentiment(i.text)))
+            tweet.insertTweet()
+
         with open(f'{screen_name} tweets.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerow(["id","created_at","text","sentiment"])
             writer.writerows(out_tweets)
-
-
 
     def all_tweets(self):
         
         for i in username:
             self.get_tweets(i)
 
+
 # Execution 
+if __name__ == '__main__':
 
-twitter_authenticator = TwitterAuthenticator()
-twitter_authenticator.twitter_auth()
+    # Create Table
+    conn = sqlite3.connect('twitter.db')
+    c = conn.cursor() 
+    c.execute('''CREATE TABLE tweets
+        (tweet_id text,
+        text text,
+        sentiment text)''')
+    conn.commit()
+    conn.close()
 
-file_ops = FileOps()
-file_ops.strip_file()
+    # Initiate DB
+    conn = sqlite3.connect('twitter.db')
+    c = conn.cursor()
 
-pull_tweets = PullTweets()
-pull_tweets.all_tweets()
+    twitter_authenticator = TwitterAuthenticator()
+    twitter_authenticator.twitter_auth()
 
+    file_ops = FileOps()
+    file_ops.strip_file()
 
-
+    pull_tweets = PullTweets()
+    pull_tweets.all_tweets()
 
 
